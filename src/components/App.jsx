c7aa86bcf1f7fdc0acc2e38877b378fa2c8fda7d
ZeMunchkin <smbrandes17@gmail.com>
2018-01-31 19:27:27 -0800

    have our videolist rendering with example data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,33 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><Search /></div>
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      videos: exampleVideoData
+    };
+    console.log('our videos', this.state.videos);
+  }
+  
+  render () {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><Search /></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <div><VideoPlayer /></div>
+          </div>
+          <div className="col-md-5">
+            <div><VideoList videos={this.state.videos}/></div>
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><VideoPlayer /></div>
-      </div>
-      <div className="col-md-5">
-        <div><VideoList videos={window.exampleVideoData}/></div>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }   
+}
+
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
