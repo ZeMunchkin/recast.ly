612b79776af598c373a4a285b91ef16b117e95e2
ZeMunchkin <smbrandes17@gmail.com>
2018-02-01 11:58:58 -0800

    Revert "rendering youtube data"
    This reverts commit 40b8c04e6da9efd2ab9a7b66314b33136a142aed.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,43 +8,20 @@ class App extends React.Component {
     console.log('our videos', this.state.videos);
   }
   
-  handleClick (video) {
+  handleClick (event, video) {
+    console.log('I was clicked!');
+    //retrieve which video key was clicked
     //update our state.video with this.setState()
-    //pass in the selected video
     this.setState({video: video});
-  }
-  
-  searchYouTube (query) {
-    var app = this;
-    $.ajax ({
-      url: 'https://www.googleapis.com/youtube/v3/search',
-      type: 'GET',
-      data: {
-        'q': query,
-        'maxResults': 5,
-        'part': 'snippet',
-        'videoEmbeddable': true,
-        'type': 'video',
-        'key': window.YOUTUBE_API_KEY,
-      },
-      success: function (data) {
-        console.log('Success! Here is the data: ', data);
-        app.setState({videos: data.items});
-        app.setState({video: data.items[0]});
-      },
-      error: function (data) {
-        console.log('Error! ', data);
-      }
-    });
+    //pass in the selected video
   }
   
   render () {
-    console.log('our current videos state: ', this.state.videos);
     return (
       <div>
         <nav className="navbar">
           <div className="col-md-6 offset-md-3">
-            <div><Search searchFunc={this.searchYouTube.bind(this)}/></div>
+            <div><Search /></div>
           </div>
         </nav>
         <div className="row">
@@ -52,7 +29,7 @@ class App extends React.Component {
             <div><VideoPlayer video={this.state.video}/></div>
           </div>
           <div className="col-md-5">
-            <div><VideoList clickFunc={this.handleClick.bind(this)} videos={this.state.videos}/></div>
+            <div><VideoList videos={this.state.videos}/></div>
           </div>
         </div>
       </div>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,17 +1,11 @@
-var Search = (props) => (
+var Search = () => (
   <div className="search-bar form-inline">
     <input className="form-control" type="text" />
-    <button onClick={() => {
-      console.log($('.form-control').val());
-      props.searchFunc($('.form-control').val());
-    }
-    } className="btn hidden-sm-down">
+    <button className="btn hidden-sm-down">
       <span className="glyphicon glyphicon-search"></span>
     </button>
   </div> 
 );
-
-
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
